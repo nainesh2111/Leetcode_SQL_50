9e66c7fe1628398518fd7c885abec477206cf3d9
Nainesh Shirsath <165702101+nainesh2111@users.noreply.github.com>
2026-03-30 12:03:00 +0530

    Add SQL query to calculate confirmation rate

---
+++ confirmation_rate.sql
@@ -0,0 +1,7 @@
+
+SELECT s.user_id,
+    ROUND(IFNULL(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(c.action), 0), 2) as confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id = c.user_id
+GROUP BY s.user_id;
